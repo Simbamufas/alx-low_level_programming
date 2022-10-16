41dfeb9e4abf229a145eb4945b63ffce7dfca0dd
Simbamufas <113668812+Simbamufas@users.noreply.github.com>
2022-10-16 22:26:48 +0300

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - starting point
+*Return: value is zero
+*/
+int main(void)
+{
+  /* start the vawulence */
+char a;
+for (a = 'a'; a <= 'z'; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
